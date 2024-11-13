5a6cfd67e4bbd1ca5d35c95a3804766443ad5241
gfuruta <gfuruta@GustavoBykonz.myguest.virtualbox.org>
2024-11-12 21:06:12 -0300

    Add main banner

---
+++ organo/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+function Banner () {
+    return (
+        <header className='banner'>
+            <img src='/images/banner.png' alt="main_banner"></img>
+        </header>
+    )
+}
+export default Banner
